98a604f0c20786c444d764b9110d8a4818b1624f
Jasjot Singh <jasjot784@gmail.com>
2020-07-02 22:10:02 +0530

    Fragment State Saved

--- app/src/main/java/com/jasjotsingh/fragmentbasic/SimpleFragment.java
+++ app/src/main/java/com/jasjotsingh/fragmentbasic/SimpleFragment.java
@@ -1,7 +1,9 @@
 package com.jasjotsingh.fragmentbasic;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -17,80 +19,116 @@ import android.widget.TextView;
  */
 public class SimpleFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    // The radio button choice has 3 states: 0 = yes, 1 = no,
+    // 2 = default (no choice). Using all 3 states.
     private static final int YES = 0;
     private static final int NO = 1;
+    private static final int NONE = 2;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // Initialize the choice to the default (no choice)
+    private int mRadioButtonChoice = NONE;
+
+    // The "choice" key for the bundle.
+    private static final String CHOICE = "choice";
+
+    // The listener interface.
+    OnFragmentInteractionListener mListener;
 
     public SimpleFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SimpleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SimpleFragment newInstance(String param1, String param2) {
-        SimpleFragment fragment = new SimpleFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    interface OnFragmentInteractionListener {
+        void onRadioButtonChoice(int choice);
     }
 
+    /**
+     * This method checks if the hosting activity has implemented
+     * the OnFragmentInteractionListener interface. If it does not,
+     * an exception is thrown.
+     *
+     * @param context
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new ClassCastException(context.toString()
+                    + getResources().getString(R.string.exception_message));
         }
     }
 
+    /**
+     * Create the fragment view by inflating the layout, and if a choice
+     * has already been made, get the choice argument from the bundle
+     * and check the appropriate choice (yes or no) in the fragment.
+     * Using a radioGroup listener, get the radio button choice from the
+     * user.
+     *
+     * @param inflater           LayoutInflater to inflate any views in the fragment
+     * @param container          ViewGroup of parent view to attach fragment
+     * @param savedInstanceState Bundle for previous state
+     * @return rootView
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        final View rootView =
-                inflater.inflate(R.layout.fragment_simple, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_simple,
+                container, false);
         final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
 
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                View radioButton = radioGroup.findViewById(checkedId);
-                int index = radioGroup.indexOfChild(radioButton);
-                TextView textView =
-                        rootView.findViewById(R.id.fragment_header);
-                switch (index) {
-                    case YES: // User chose "Yes."
-                        textView.setText(R.string.yes_message);
-                        break;
-                    case NO: // User chose "No."
-                        textView.setText(R.string.no_message);
-                        break;
-                    default: // No choice made.
-                        // Do nothing.
-                        break;
-                }
+        // If user reopens the fragment after making a choice, the bundle
+        // contains "choice".
+        if (getArguments().containsKey(CHOICE)) {
+            // A choice was made, so get the choice.
+            mRadioButtonChoice = getArguments().getInt(CHOICE);
+            // Check the radio button choice.
+            if (mRadioButtonChoice != NONE) {
+                radioGroup.check
+                        (radioGroup.getChildAt(mRadioButtonChoice).getId());
             }
-        });
+        }
 
+        // Set the radioGroup onCheckedChanged listener.
+        radioGroup.setOnCheckedChangeListener(
+                new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                        View radioButton = radioGroup.findViewById(checkedId);
+                        int index = radioGroup.indexOfChild(radioButton);
+                        TextView textView =
+                                rootView.findViewById(R.id.fragment_header);
+                        switch (index) {
+                            case YES: // User chose "Yes".
+                                textView.setText(R.string.yes_message);
+                                mRadioButtonChoice = YES;
+                                mListener.onRadioButtonChoice(YES);
+                                break;
+                            case NO: // User chose "No".
+                                textView.setText(R.string.no_message);
+                                mRadioButtonChoice = NO;
+                                mListener.onRadioButtonChoice(NO);
+                                break;
+                            default: // No choice made.
+                                mRadioButtonChoice = NONE;
+                                mListener.onRadioButtonChoice(NONE);
+                                break;
+                        }
+                    }
+                });
+
+        // Return the View for the fragment's UI.
         return rootView;
     }
-    public static SimpleFragment newInstance() {
-        return new SimpleFragment();
+
+    public static SimpleFragment newInstance(int choice) {
+        SimpleFragment fragment = new SimpleFragment();
+        Bundle arguments = new Bundle();
+        arguments.putInt(CHOICE, choice);
+        fragment.setArguments(arguments);
+        return fragment;
     }
 }
